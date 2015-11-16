f6bf38e8268750a747949cca1d739dd5731d5d81
Sascha Can <Sascha.Can@googlemail.com>
2015-11-16 14:05:05 +0100

    removed duplication in dependencies

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     version='0.3.0',
     packages=['corputil'],
     package_data={'corputil': ['stopwords/*.txt']},
+    install_requires=['nltk'],
     url='',
     license='MIT',
     author='Sascha Can',
     author_email='Sascha.Can@gmail.com',
-    description='',
-    install_requires=['nltk']
+    description=''
 )

338657f6d6af5ae844181be6a16017cabe81698e
Sascha Can <Sascha.Can@googlemail.com>
2015-11-27 10:33:22 +0100

    updated stoplist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='corputil',
-    version='0.4.1',
+    version='0.4.2',
     packages=['corputil'],
     package_data={'corputil': ['stopwords/*.txt']},
     install_requires=['nltk'],

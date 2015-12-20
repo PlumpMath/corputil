6adde265622fb171b33b26b0d3669a7218d8d16d
Sascha Can <Sascha.Can@googlemail.com>
2015-12-20 13:08:24 +0100

    added stopwords loader

--- corputil/utils.py
+++ corputil/utils.py
@@ -10,3 +10,12 @@ def get_stopwords(sw):
 def get_tokenizer(lang):
     model = 'tokenizers/punkt/{}.pickle'.format(lang.lower())
     return nltk.data.load(model)
+
+
+def load_stopwords(file_path, encoding='UTF-8'):
+    with open(file_path, 'r', encoding=encoding) as f:
+        stopwords = f.read().split('\n')[:-1]
+    if stopwords:
+        return stopwords
+    else:
+        return []
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='corputil',
-    version='0.6.0',
+    version='0.6.1',
     packages=['corputil'],
     package_data={'corputil': ['stopwords/*.txt']},
     install_requires=['nltk'],
